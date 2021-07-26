74b1e2c03e662fee057ef199502f4de537c775cf
kamran.ali <kamran.ali@NP-SOD-Kamran-LAP.nayapay.com>
2021-07-26 23:59:41 +0500

    Initial files of the project.

---
+++ src/main/java/com/codediverse/lamdaexpressions/LamdaExpressionsApplication.java
@@ -0,0 +1,13 @@
+package com.codediverse.lamdaexpressions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LamdaExpressionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LamdaExpressionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codediverse/lamdaexpressions/LamdaExpressionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codediverse.lamdaexpressions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LamdaExpressionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
